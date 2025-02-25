a71b8b01fd5c82da923a613540a6fe8ba9e13468
Oscar Sagemo <OscarS@bmsystem.se>
2025-02-25 12:27:06 +0100

    config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,6 +6,7 @@ return {
         require("nvim-treesitter.configs").setup({
             -- A list of parser names, or "all"
             ensure_installed = {
+                "bash",
                 "c",
                 "diff",
                 "gitcommit",
