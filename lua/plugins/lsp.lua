6542ba21d00701cc4c11772aab5cf5a50e66d7a2
Oscar Sagemo <OscarS@bmsystem.se>
2025-02-25 13:11:47 +0100

    make diagnostics focusable by open float twice

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -109,7 +109,8 @@ return {
                     vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
                     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
                     vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-                    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+                    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float({ focusable = true }) end,
+                        opts)
                     vim.keymap.set('n', '<leader>dl', vim.diagnostic.setloclist)
                     vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, opts)
                     vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end, opts)
