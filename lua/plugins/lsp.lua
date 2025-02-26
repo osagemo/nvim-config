f428dabc48f8665fd46e95dd37602f9757b84c4e
Oscar Sagemo <OscarS@bmsystem.se>
2025-02-26 15:33:41 +0100

    -

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -223,6 +223,7 @@ return {
                     json = { 'prettierd', "prettier" },
                     html = { 'prettierd', "prettier" },
                     css = { 'prettierd', "prettier" },
+                    yaml = { 'prettierd', "prettier" },
                 },
                 format_on_save = {
                     -- These options will be passed to conform.format()
